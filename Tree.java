5710dd0d920cfd7f029bd569ea9bdc194c4d5123
musavirudeen <musavirudeen@gmail.com>
2023-12-28 22:31:23 +0530

    Corrected mistakes in it

--- Tree.java
+++ Tree.java
@@ -1,5 +1,5 @@
 public class Tree{
   public static void main(String[] args){
-    System.out.prinln("Hey Hi... This is my first java file on github");
+    System.out.println("Hey Hi... This is my first java file on github");
   }
 }
